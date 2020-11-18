b0c8f0ce66d0e2751d39a311cbf910a49484f76f
over40dev <ted@over40.dev>
2020-11-18 17:14:19 -0330

    m06-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import AppComponent from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   bootstrap: [
@@ -11,6 +13,8 @@ import AppComponent from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    FontAwesomeModule,
   ],
 })
 export class AppModule { }
