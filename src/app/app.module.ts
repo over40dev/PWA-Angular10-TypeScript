f8ad5cf26e1b638d98fef0d734273168a716e3df
over40dev <ted@over40.dev>
2020-11-23 20:46:51 -0330

    m14-extract-app-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import AppComponent from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { QuestionComponent } from './question/question.component';
+import { NotificationComponent } from './notification/notification.component';
+import { ScoreComponent } from './score/score.component';
+import { CardRowComponent } from './card-row/card-row.component';
+import { CardComponent } from './card-row/card/card.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   bootstrap: [
@@ -10,6 +16,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   declarations: [
     AppComponent,
+    QuestionComponent,
+    NotificationComponent,
+    ScoreComponent,
+    CardRowComponent,
+    CardComponent,
+    TimerComponent,
   ],
   imports: [
     BrowserModule,
