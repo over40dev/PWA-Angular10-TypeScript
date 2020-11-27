03317ba2f92d7414f4123274379d38cc6ff07ab3
over40dev <ted@over40.dev>
2020-11-27 17:28:21 -0330

    04-added styles property to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<p>Hello World</p>'
+  template: '<p>Hello World</p>',
+  styles: ['p {font-size: 30px; color: red; text-align: center; }'],
 })
 export class AppComponent {}
