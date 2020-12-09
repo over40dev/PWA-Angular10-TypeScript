55ed74ac3e6ce0cbe67cdac9971d4dfd61b51ecb
over40dev <ted@over40.dev>
2020-12-08 22:35:54 -0330

    m11 mods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// c14-02 (import FaIconLirary)
-import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-// c14-03 (import Icon we want to use in our app FaIconLirary)
-import { faTrophy } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { QuestionComponent } from './question/question.component';
 import { NotificationComponent } from './notification/notification.component';
 import { ScoreComponent } from './score/score.component';
@@ -32,9 +29,4 @@ import { TimerComponent } from './timer/timer.component';
     FontAwesomeModule,
   ],
 })
-export class AppModule {
-  // c14-04 (setup constructor to inject FA Icon Library to get access to our chosen icons)
-  // constructor(library: FaIconLibrary) {
-  //   library.addIcons(faTrophy);
-  // }
-}
+export class AppModule { }
