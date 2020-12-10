0c1598fe4c80387988ca8bb15f5fb37cac8dbacf
over40dev <ted@over40.dev>
2020-12-10 14:35:45 -0330

    18-Dependency-Injection

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -3,4 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class GameService {}
+export class GameService {
+  private score = 15;
+
+  public getScore(): number {
+    return this.score;
+  }
+}
